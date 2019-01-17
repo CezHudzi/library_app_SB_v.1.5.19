d17cff59c6b4d154fbf19345146e4e446a041465
CezHudzi <hudzik.cezary@gmail.com>
2019-01-17 18:58:07 +0100

    SOLVED PROBLEM WITH SET

--- src/main/java/com/application/library/mapperDTO/bookmapper/BookCreateDTO.java
+++ src/main/java/com/application/library/mapperDTO/bookmapper/BookCreateDTO.java
@@ -3,14 +3,30 @@ package com.application.library.mapperDTO.bookmapper;
 
 
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "bookName",
+        "author"
+})
 public class BookCreateDTO {
 
 
-
+    @JsonProperty("bookName")
     private String bookName;
+    @JsonProperty("author")
     private Set<Integer> author;
+
+    public BookCreateDTO() {
+    }
 
     public BookCreateDTO(String bookName, Set<Integer> author) {
         this.bookName = bookName;
@@ -22,7 +38,8 @@ public class BookCreateDTO {
     }
 
 
-    public Set<Integer> getAuthorSet() {
+    public Set<Integer> getAuthorSet()
+    {
         return author;
     }
 }
